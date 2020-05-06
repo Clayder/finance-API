f92f4e0337e0b34bafb37e10c7f2e6e63c42d4a4
Clayder <peterclayder@gmail.com>
2020-05-06 19:52:52 -0300

    [exceptions] Criei a estrutura para gerar exceptions

---
+++ src/main/java/com/clayder/Finances/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.clayder.Finances.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.clayder.Finances.services.exceptions.ObjectNotFoundException;
+
+// Interceptador de exceções geradas por métodos anotados com @RequestMapping
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	// Tratamento de exceções do tipo ObjectNotFoundException
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
