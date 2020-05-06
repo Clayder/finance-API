9a005c094b1655522904e885344cea125cd1e2e8
Clayder <peterclayder@gmail.com>
2020-05-06 07:55:58 -0300

    [card] Criei a estrutura do service

---
+++ src/main/java/com/clayder/Finances/services/CreditCardService.java
@@ -0,0 +1,16 @@
+package com.clayder.Finances.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.clayder.Finances.domain.CreditCard;
+import com.clayder.Finances.repositories.ICreditCardRepository;
+
+@Service
+public class CreditCardService {
+	
+	@Autowired
+	ICreditCardRepository repository;
+	
+
+}
