76a6dad8627b9bde127f91cedac0fe71e6be1351
Clayder <peterclayder@gmail.com>
2020-05-09 11:13:21 -0300

    [exceptions] Tratando a exceção para excluir foreign.

--- src/main/java/com/clayder/Finances/services/CreditCardService.java
+++ src/main/java/com/clayder/Finances/services/CreditCardService.java
@@ -3,10 +3,12 @@ package com.clayder.Finances.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.clayder.Finances.domain.CreditCard;
 import com.clayder.Finances.repositories.ICreditCardRepository;
+import com.clayder.Finances.services.exceptions.DataIntegrityException;
 import com.clayder.Finances.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -33,6 +35,11 @@ public class CreditCardService {
 	
 	public void delete(Long id) {
 		this.getById(id);
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cartão que possui fatura vinculada.");
+		}
 	}
 }
