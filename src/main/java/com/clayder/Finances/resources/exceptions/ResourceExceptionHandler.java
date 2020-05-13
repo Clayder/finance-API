c1584e915d1049028052dc8efc290213170eecba
Clayder <peterclayder@gmail.com>
2020-05-13 20:41:13 -0300

    [exceptions] Tratei a exception 405

--- src/main/java/com/clayder/Finances/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/clayder/Finances/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -46,6 +47,14 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
+	
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<StandardError> httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.METHOD_NOT_ALLOWED.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(err);
+	}
+	
+	
 	
 	
 }
