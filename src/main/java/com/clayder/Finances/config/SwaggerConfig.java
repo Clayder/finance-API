ebd585a9a5b38b227dbcb96c36b2e645e271a1d1
Clayder <peterclayder@gmail.com>
2020-05-13 23:53:56 -0300

    [swagger] Configurei o swagger com JWT

--- src/main/java/com/clayder/Finances/config/SwaggerConfig.java
+++ src/main/java/com/clayder/Finances/config/SwaggerConfig.java
@@ -1,49 +1,108 @@
 package com.clayder.Finances.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.models.auth.In;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {                                    
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))    
+          .paths(PathSelectors.any())
+          .build()
+          .useDefaultResponseMessages(false)                                   
+          .securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
+          .securityContexts(Arrays.asList(securityContext()))
+          .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Simple Spring Boot REST API")
+                .description("\"Um exemplo de aplicação Spring Boot REST API\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Wladimilson", "https://treinaweb.com.br", "contato@treinaweb.com.br"))
+                .build();
+    }
+
+    private List<ResponseMessage> responseMessageForGET()
+    {
+        return new ArrayList<ResponseMessage>() {
+            private static final long serialVersionUID = 1L;
+
+            {
+            add(new ResponseMessageBuilder()   
+                .code(500)
+                .message("500 message")
+                .responseModel(new ModelRef("Error"))
+                .build());
+            add(new ResponseMessageBuilder() 
+                .code(403)
+                .message("Forbidden!")
+                .build());
+        }};
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .forPaths(PathSelectors.ant("/api/v1/**"))
+            .build();
+    }
+    
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+            = new AuthorizationScope("ADMIN", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(
+            new SecurityReference("Token Access", authorizationScopes));
+    }
+
+    //http://localhost:8080/swagger-ui.html 
+    //.apis(RequestHandlerSelectors.basePackage("br.com.ciceroednilson"))          
+}
 
 
-	  @Bean
-	    public Docket api() {
-	        return new Docket(DocumentationType.SWAGGER_2)
-	          .select()
-	          .apis(RequestHandlerSelectors.any())
-	          .paths(PathSelectors.any())
-	          .build();
-	    }
 
-	  private ApiInfo metaData() {
-	    return new ApiInfoBuilder()
-	        .title("Spring Boot REST API")
-	        .description("\"Spring Boot REST API for greeting people\"")
-	        .version("1.0.0")
-	        .license("Apache License Version 2.0")
-	        .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-	        .build();
-	  }
-	  
-	  
 
-	  @Override
-	  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-	    registry.addResourceHandler("swagger-ui.html")
-	        .addResourceLocations("classpath:/META-INF/resources/");
 
-	    registry.addResourceHandler("/webjars/**")
-	        .addResourceLocations("classpath:/META-INF/resources/webjars/");
-	  }
-	}
+
+
+
+
+
+
+
+
+
+
+
