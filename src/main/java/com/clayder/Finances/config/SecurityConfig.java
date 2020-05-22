af5e68b5ccfcd7a3320235c8f3ea85d3ad28f87b
Clayder <peterclayder@gmail.com>
2020-05-21 22:27:14 -0300

    [security] autorização implementada.

--- src/main/java/com/clayder/Finances/config/SecurityConfig.java
+++ src/main/java/com/clayder/Finances/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.clayder.Finances.security.JWTAuthenticationFilter;
+import com.clayder.Finances.security.JWTAuthorizationFilter;
 import com.clayder.Finances.security.JWTUtil;
 
 @Configuration
@@ -53,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		 */
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHES).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
