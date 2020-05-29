dd5011fe00f747ec3efc256b6fa1cd37a225d183
Clayder <peterclayder@gmail.com>
2020-05-29 11:33:28 -0300

    TDD init

---
+++ financestdd/src/main/java/com/clayder/financestdd/FinancestddApplication.java
@@ -0,0 +1,13 @@
+package com.clayder.financestdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancestddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancestddApplication.class, args);
+	}
+
+}
---
+++ financestdd/src/test/java/com/clayder/financestdd/FinancestddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clayder.financestdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancestddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
