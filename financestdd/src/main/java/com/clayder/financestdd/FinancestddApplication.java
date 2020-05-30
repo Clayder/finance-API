866df36a9b77383eb0070ec8c32bb97847b388ad
Clayder <peterclayder@gmail.com>
2020-05-30 14:32:41 -0300

    Instalei e configurei a biblioteca modelmapper

--- financestdd/src/main/java/com/clayder/financestdd/FinancestddApplication.java
+++ financestdd/src/main/java/com/clayder/financestdd/FinancestddApplication.java
@@ -1,11 +1,17 @@
 package com.clayder.financestdd;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FinancestddApplication {
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FinancestddApplication.class, args);
 	}
