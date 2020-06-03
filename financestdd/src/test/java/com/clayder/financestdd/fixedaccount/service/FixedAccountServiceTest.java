b8bb60a48c11807096f92f63bdffb43ba2364e16
Clayder <peterclayder@gmail.com>
2020-06-02 22:45:37 -0300

    [TDD] testando se é possível cadastrar conta fixa com nome duplicado.

--- financestdd/src/test/java/com/clayder/financestdd/fixedaccount/service/FixedAccountServiceTest.java
+++ financestdd/src/test/java/com/clayder/financestdd/fixedaccount/service/FixedAccountServiceTest.java
@@ -1,9 +1,11 @@
 package com.clayder.financestdd.fixedaccount.service;
 
+import com.clayder.financestdd.api.exceptions.type.BusinessException;
 import com.clayder.financestdd.api.fixedaccount.model.entity.FixedAccount;
 import com.clayder.financestdd.api.fixedaccount.model.repository.IFixedAccountRepository;
 import com.clayder.financestdd.api.fixedaccount.service.FixedAccountService;
 import com.clayder.financestdd.api.fixedaccount.service.IFixedAccountService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -67,5 +69,40 @@ public class FixedAccountServiceTest {
         assertThat(save.getOwner()).isEqualTo("Peter");
         assertThat(save.getPaymentDay()).isEqualTo(22);
         assertThat(save.getPrice()).isEqualTo(54.99);
+    }
+
+    @Test
+    @DisplayName("Deve lançar erro ao tentar salvar uma conta fixa com nome duplicado.")
+    public void shouldNotSaveAccountWithDuplicatedName(){
+        // cenario
+        FixedAccount account = createValidAccount();
+
+        /**
+         * Quando for executado o método existsByName do repository, retornar true
+         * dessa forma vamos conseguir simular que já essa conta fixa cadastrada.
+         */
+        Mockito.when( repository.existsByName(Mockito.anyString()) ).thenReturn(true);
+
+        //execução
+        Throwable exception = Assertions.catchThrowable(() -> service.save(account));
+
+        // verificações
+        assertThat(exception)
+                .isInstanceOf(BusinessException.class)
+                .hasMessage("Conta fixa já cadastrada");
+
+        /**
+         * Verifica que nunca será executado o método save
+         */
+        Mockito.verify(repository, Mockito.never()).save(account);
+    }
+
+    private FixedAccount createValidAccount(){
+        return FixedAccount.builder()
+				.name("Vivo")
+				.owner("Peter")
+				.paymentDay(22)
+				.price(54.99)
+				.build();
     }
 }
