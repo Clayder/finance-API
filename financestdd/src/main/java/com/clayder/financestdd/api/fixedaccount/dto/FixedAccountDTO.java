b3d9f09b966b2650369af9e6dd0d644e458c7948
Clayder <peterclayder@gmail.com>
2020-06-25 20:26:09 -0300

    Realizando validação dos dados das contas fixas.

--- financestdd/src/main/java/com/clayder/financestdd/api/fixedaccount/dto/FixedAccountDTO.java
+++ financestdd/src/main/java/com/clayder/financestdd/api/fixedaccount/dto/FixedAccountDTO.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 
 @Getter
 @Setter
@@ -19,15 +19,20 @@ public class FixedAccountDTO {
     private Integer id;
 
     @NotEmpty
+    @Length(min=2, max=50)
     private String name;
 
     @NotNull
+    @DecimalMin(value = "0.1", inclusive = true)
     private double price;
 
     @NotNull
+    @Min(1)
+	@Max(31)
     private Integer paymentDay;
 
     @NotEmpty
+    @Length(min=5, max=50)
     private String owner;
 
 }
